ae96e61627a9685082a9ff41340ab8019e8c8179
Chase Agee <chase.agee@gmail.com>
2024-04-19 14:28:03 -1000

    fixed a mistake in unit test 2

--- client_test.py
+++ client_test.py
@@ -19,7 +19,7 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
     for quote in quotes:
-       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']/2)))
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
